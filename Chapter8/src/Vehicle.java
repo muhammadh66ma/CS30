f043760a3b9c76946b6602bf43cabcb0d3583066
54242001 <54242001@L815WD150042.edu.cbe.ab.ca>
2022-03-18 10:21:26 -0600

    Done

--- Chapter8/src/Vehicle.java
+++ Chapter8/src/Vehicle.java
@@ -1,5 +1,45 @@
-
-public class Vehicle {
+abstract class Vehicle {
+	private int fuelEconomyCity, fuelEconomyHwy;
+	private int seatingCapacity;
+	private double cargoVolume;
 
 	
+	public Vehicle(int fECity, int fEHwy, int seating, double cargo) {
+		fuelEconomyCity = fECity;
+		fuelEconomyHwy = fEHwy;
+		seatingCapacity = seating;
+		cargoVolume = cargo;
+	}
+	
+		
+
+	 public int getFEHwy() {
+	 	return(fuelEconomyHwy);
+	 }
+
+
+
+	 public int getFECity() {
+	 	return(fuelEconomyCity);
+	 }
+
+
+	
+	 public int getSeating() {
+	 	return(seatingCapacity);
+	 }
+
+
+	
+	 public double getCargoVolume() {
+	 	return(cargoVolume);
+	 }
+
+
+
+	abstract String cargoContainer();
+	
+	
+	
+	abstract String description();
 }
